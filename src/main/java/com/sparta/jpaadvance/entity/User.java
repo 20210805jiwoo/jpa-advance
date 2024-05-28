f392593efcd12caadb2aa7a1176589e4db28e019
jiwoo <1114jiwoo@gmail.com>
2024-05-28 17:00:36 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,6 +17,6 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
-    private List<Order> orderList = new ArrayList<>();
+    @OneToMany(mappedBy = "user") // 외래키의 주인 지정
+    private List<Food> foodList = new ArrayList<>();
 }
