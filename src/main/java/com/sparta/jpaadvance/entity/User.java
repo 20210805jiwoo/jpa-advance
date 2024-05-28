a438c4d46a3398222ea821958e091d27ede5c7d8
jiwoo <1114jiwoo@gmail.com>
2024-05-28 17:10:54 +0900

    영속성 전이

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,6 +17,11 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user") // 외래키의 주인 지정
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}) // 외래키의 주인 지정
     private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // 외래키 설정
+    }
 }
